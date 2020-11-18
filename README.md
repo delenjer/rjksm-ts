7627138594eedf78b0ff70216ca69d86779b1653
delenjer <delenjer@gmail.com>
2020-11-18 16:26:58 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Rjksm TS
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => (
+  <div className="App">
+    <h1>React rjksm TS</h1>
+  </div>
+);
+
+export default App;
