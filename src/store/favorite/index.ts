3e46f789f5a648e4e6649ce723db0c55ea6ebd3f
delenjer <delenjer@gmail.com>
2020-12-18 23:41:19 +0200

    add sraniy favorite

---
+++ src/store/favorite/index.ts
@@ -0,0 +1,16 @@
+import { IActionIsLoading } from '../../interface/interface';
+import { SET_LOADING } from './actions';
+
+export const getIsLoading = (state: boolean) => state;
+
+const loadReducer = (isLoading = false, action:IActionIsLoading) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return action.isLoading;
+
+    default:
+      return isLoading;
+  }
+}
+
+export default loadReducer;
